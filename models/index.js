2f0abf0e871c80f67e2add8311c1a6f92ca53de9
Dylan McAulay <dmcaulay97@gmail.com>
2021-06-25 22:13:01 -0400

    added all html templates. added schema

---
+++ models/index.js
@@ -0,0 +1,15 @@
+const User = require('./User');
+const Blog = require('./Blog');
+const Comment = require('./Comment');
+
+//User has many Blog
+
+//Blog belongs to Use
+
+//User has many Comment
+
+//Blog has many Comment
+
+//comment belongs to user
+
+//comment belongs to blog
